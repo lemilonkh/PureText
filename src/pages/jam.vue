bdde95c0c1826431e3dff5f399fb3232adc37aeb
Milan Gruner <milangruner@gmail.com>
2018-03-27 21:32:00 +0200

    Revert "Add package vue-grid-layout and use it on Jam page"
    This reverts commit 123c78111575ebb196cb6865910f5566125879c5.

--- src/pages/jam.vue
+++ src/pages/jam.vue
@@ -1,37 +1,22 @@
 <template>
   <q-page class="flex flex-center fit round-borders">
-    <grid-layout
-      :layout="layout"
-      :col-num="12"
-      :row-height="30"
-      :is-draggable="true"
-      :is-resizable="false"
-      :is-mirrored="false"
-      :vertical-compact="true"
-      :margin="[10, 10]"
-      :use-css-transforms="true"
+    <q-table
+      title="Jam"
+      row-key="name"
+      :data="tableData"
+      :columns="columns"
+      :hide-bottom="true"
     >
-      <grid-item
-        v-for="item in layout"
-        :x="item.x"
-        :y="item.y"
-        :w="item.w"
-        :h="item.h"
-        :i="item.i"
-        :autoSize="false">
-        <div style="width: 50px; height: 50px; border: 1px solid; margin: 10px">
-          {{item.i}}
-        </div>
-      </grid-item>
-    </grid-layout>
+      <q-tr slot="body" slot-scope="props" :props="props" @click.native="rowClick(props.row)" class="cursor-pointer">
+        <q-td v-for="col in props.cols" :style="{width: col.width + 'px'}" :key="col.name" :props="props">
+          Track {{col.name}}
+        </q-td>
+      </q-tr>
+    </q-table>
   </q-page>
 </template>
 
 <script>
-import VueGridLayout from 'vue-grid-layout'
-let GridLayout = VueGridLayout.GridLayout
-let GridItem = VueGridLayout.GridItem
-
 const rowCount = 4
 const volumeWidth = 200
 const mutedWidth = 40
@@ -65,24 +50,12 @@ for (let i = 0; i < rowCount; i++) {
   })
 }
 
-let layout = [
-  {x: 1, y: 1, w: 1, h: 1, i: 0},
-  {x: 2, y: 1, w: 1, h: 1, i: 1},
-  {x: 3, y: 1, w: 1, h: 1, i: 2},
-  {x: 1, y: 2, w: 2, h: 1, i: 3}
-]
-
 export default {
   name: 'Jam',
   data: () => ({
     columns,
-    tableData,
-    layout
+    tableData
   }),
-  components: {
-    GridLayout,
-    GridItem
-  },
   methods: {
     rowClick (row) {
       console.log('Click', row)
